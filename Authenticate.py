19ab59ffdbe6227552fc75a0c4746fcd78ce16de
tpblaster <promalcolm@gmail.com>
2020-02-10 09:34:31 -0500

    Reformatted handling of TCG API Auth

--- Authenticate.py
+++ Authenticate.py
@@ -4,27 +4,25 @@ import json
 
 def authenticate_tcgplayer(client_public, client_secret):
     url = "https://api.tcgplayer.com/token"
-    payload = "grant_type=client_credentials&client_id=" + str(client_public)+"&client_secret=" + str(client_secret)
-    headers = {
-        'Content-Type': "application/json",
-        'User-Agent': "PostmanRuntime/7.20.1",
-        'Accept': "*/*",
-        'Cache-Control': "no-cache",
-        'Postman-Token': "cbd44805-6be4-48b3-a9ea-3db105bfdf94,45208b7b-c801-4ea3-a65b-5dfc27c56d3e",
-        'Host': "api.tcgplayer.com",
-        'Accept-Encoding': "gzip, deflate",
-        'Content-Length': "127",
-        'Connection': "keep-alive",
-        'cache-control': "no-cache"
-    }
-
-    response = requests.request("POST", url, data=payload, headers=headers)
+    payload = "grant_type=client_credentials&client_id=" + str(client_public) + "&client_secret=" + str(client_secret)
+    response = requests.request("POST", url, data=payload)
     data = json.loads(response.text)
-    if data["token_type"] == "bearer":
+    if "access_token" in response.text:
         return data["access_token"]
         print("auth successful")
     else:
         return 0
 
 
-
+def authenticate_tcgplayer_store(client_public, client_secret, access_token):
+    bearer = authenticate_tcgplayer(client_public, client_secret)
+    if bearer != 0:
+        url = "http://api.tcgplayer.com/v1.32.0/app/authorize/" + str(access_token)
+        headers = {
+            'Authorization': "Bearer " + bearer
+        }
+        response = requests.request("POST", url, headers=headers)
+        print(response.text)
+    else:
+        print("Store Auth Failed")
+        return 0
